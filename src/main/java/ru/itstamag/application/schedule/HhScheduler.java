6812ff540ddb6e442460d5eb3f963aafb7159983
Merkurev Sergei <MerkurevSergei@yandex.ru>
2023-03-19 21:11:12 +0900

    Fill Experience on startup

--- src/main/java/ru/itstamag/application/schedule/HhScheduler.java
+++ src/main/java/ru/itstamag/application/schedule/HhScheduler.java
@@ -1,7 +1,10 @@
 package ru.itstamag.application.schedule;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.itstamag.application.dao.entity.Experience;
@@ -36,6 +39,7 @@ public class HhScheduler {
         log.info("HhScheduler.logInfo() completed: counter = {}", hhCounterEntity);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
     @Scheduled(cron = "0 0 0 * * ?")
     public void experience() {
         log.info("HhScheduler.experience() started");
